9704ae88af66eccd15c15ffc3533b498aa0eeafb
Elad Avrahami <86184072+EladAvrahami@users.noreply.github.com>
2025-02-10 01:25:56 +0200

    Add files via upload

---
+++ Arduino dino.cpp
@@ -0,0 +1,28 @@
+//https://trex-runner.com/
+#include <Servo.h>
+#define threshold  400   //sensor sensitivity
+//servo motor range in press
+#define unpress_angle  80
+#define press_angle  100
+
+
+Servo myservo;  // create servo object to control the motor
+bool trig = true;
+
+void setup() {
+    myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+    myservo.write(unpress_angle);
+}
+
+void loop() {
+
+    myservo.write(unpress_angle);              // set the motor angle to be far from the button
+    delay(1);                                 // after indicate changes in light delay 
+    if (analogRead(A0) < threshold)             // if the arduino indicates decrement less than 400  
+    {
+
+        myservo.write(press_angle);          // press the button
+        delay(100);                       // waits 100ms for the servo to reach the position
+
+    }
+}
